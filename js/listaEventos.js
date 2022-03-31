ce3ae29e4afbc5532e867667e6fee87d9954446c
Felipe Matheus <felipe774@gmail.com>
2022-03-31 13:44:42 -0300

    adicionanco arquivos everton

---
+++ js/listaEventos.js
@@ -0,0 +1,36 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const configuracao = {
+    method: 'GET',
+    headers: {
+        "Content-Type": "application/json",
+     },
+    redirect: 'follow'
+}  
+
+
+const table = document.querySelector(".table")
+const listarEventos = async () => {
+    const resposta = await fetch (`${BASE_URL}/events`, configuracao)
+    const eventos = await resposta.json();
+    const htmlEventos = eventos.map((dados, index) => {
+        const dataEvento = new Date(dados.scheduled);
+        const linhaEvento =  
+        `<tr>
+        <th scope="row">${eventos.indexOf(dados)+1}</th>
+        <td>${dataEvento.toLocaleDateString()} ${dataEvento.toLocaleTimeString().slice(0, -3)}</td>
+        <td>${dados.name}</td>
+        <td>${dados.attractions}</td>
+        <td>
+            <a href="reservas.html?id=${dados._id}&eventName=${dados.name}" class="btn btn-dark">ver reservas</a>
+            <a href="editar-evento.html?id=${dados._id}" class="btn btn-secondary">editar</a>
+            <a href="excluir-evento.html?id=${dados._id}" class="btn btn-danger">excluir</a>
+
+        </td>
+        </tr>`;
+        return linhaEvento;
+    })
+    return htmlEventos;
+}
+
+const eventos = listarEventos().then((lista) => {table.innerHTML += lista.slice(0,2)})
---
+++ js/todosEventos.js
@@ -0,0 +1,29 @@
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+
+const optionEvents = {
+    method:"GET",
+    headers: {
+        "Content-Type": "application/json"
+    },
+};
+
+
+const cards = document.querySelector("#card")
+const listarEventos = async () => {
+    const resposta = await fetch (`${BASE_URL}/events`, optionEvents)
+    const eventos = await resposta.json();
+    const htmlEventos = eventos.map((evento, index) => {
+        const dataEvento = new Date(evento.scheduled);
+        const informaçoes = `
+        <h2>${evento.name} - ${dataEvento.toLocaleDateString()} </h2>
+        <h4>${evento.attractions}</h4>
+        <p>${evento.description}</p>
+        <a href="#" class="btn btn-primary">reservar ingresso</a>
+
+        `;
+        return informaçoes;
+    })
+    return htmlEventos;
+}
+
+const eventos = listarEventos().then((resp) => {cards.innerHTML += resp.slice(0,2);});
